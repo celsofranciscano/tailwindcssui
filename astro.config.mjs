ca9462ae5285296548add2dd8c68df72d4a57189
Celso Franciscano <celsofranciscano24@gmail.com>
2024-03-11 15:18:54 -0400

    Se agrego la configuracion para deploy ruta

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+// https://celsofranciscano.github.io/tailwindcssui/
 export default defineConfig({
-	site: 'https://celsofranciscano.github.io',
+	site: 'https://celsofranciscano.github.io/tailwindcssui/',
 	integrations: [mdx(), sitemap()],
 });
