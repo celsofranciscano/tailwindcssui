2868cd981fb253240df2160f05e735619f8f55ec
Celso Franciscano <celsofranciscano24@gmail.com>
2024-03-12 18:49:01 -0400

    Primer nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-// https://celsofranciscano.github.io/tailwindcssui/
 export default defineConfig({
-	site: 'https://celsofranciscano.github.io/tailwindcssui/',
-	base: '/tailwindcssui',
-	integrations: [mdx(), sitemap()],
+  site: 'https://celsofranciscano.github.io/tailwindcssui/',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
